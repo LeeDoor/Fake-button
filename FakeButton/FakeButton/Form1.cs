9c635e25d207058923d1b1338ec7badc3890d1d8
Leonid Samoshenko <samoshenkoleonid@yandex.ru>
2022-07-04 16:21:22 +0300

    strange moonwalk

--- FakeButton/FakeButton/Form1.cs
+++ FakeButton/FakeButton/Form1.cs
@@ -8,6 +8,7 @@ namespace FakeButton
             Y = MousePosition.Y - Location.Y -30
         }; 
         }
+        private readonly double reacDist = 150d;
 
         public Form1()
         {
@@ -16,27 +17,68 @@ namespace FakeButton
 
         private double GetDistance (Point point, Control control)
         {
-            Point point2 = new Point() 
-            { 
-                X = control.Location.X + control.Width / 2, 
-                Y = control.Location.Y + control.Height / 2 
-            };
-            Point distance = new Point() 
-            {
-                X = Math.Abs(point.X - point2.X), 
-                Y = Math.Abs(point.Y - point2.Y) 
-            };
+            Point point2 = GetControlMidpoint(control);
+            Point distance = GetVector(point, point2);
             return Math.Sqrt(Math.Pow(distance.X,2) + Math.Pow(distance.Y, 2));
         }
 
+        private Point GetControlMidpoint(Control control)
+        {
+            return new Point()
+            {
+                X = control.Location.X + control.Width / 2, 
+                Y = control.Location.Y + control.Height / 2
+            };
+        }
+        private Point GetVector(Point point, Point point2)
+        {
+            return new Point()
+            {
+                X = Math.Abs(point.X - point2.X), 
+                Y = Math.Abs(point.Y - point2.Y)
+            };
+        }
+
+
         private void OnMouseMove(object sender, MouseEventArgs e)
         {
-            RightButton.Size = new Size() { Height = RightButton.Size.Height, Width = (int)GetDistance(InMousePosition, WrongButton) };
+            WrongButton.Text = GetDistance(InMousePosition, WrongButton).ToString();
+            if (GetDistance(InMousePosition, WrongButton) < reacDist)
+            {
+                MoveOutwards(WrongButton);
+            }
+        }
+
+        private void MoveOutwards(Control control)
+        {
+            Point backVector = GetVector(GetControlMidpoint(control), InMousePosition);
+            InvertVector(backVector);
+            ApplyMovement(control, backVector);
+        }
+
+        private Point InvertVector (Point point)
+        {
+            return new Point()
+            {
+                X = -point.X,
+                Y = -point.Y
+            };
+        }
+
+        private void ApplyMovement(Control control, Point vector)
+        {
+            control.Location = new Point()
+            {
+                X = control.Location.X + vector.X,
+                Y = control.Location.Y + vector.Y
+            };
         }
 
         private void Form1_MouseDown(object sender, MouseEventArgs e)
         {
 
         }
+
+
     }
 }
